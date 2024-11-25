27518e06cf8ec96ec43059d042549250c82a7e3e
Asraful Islam <asraful009@gmail.com>
2024-11-25 16:43:20 +0600

    box sit on table

--- Source/UE5T5/Private/UE5T5BoxTriggerComponent.cpp
+++ Source/UE5T5/Private/UE5T5BoxTriggerComponent.cpp
@@ -5,9 +5,60 @@
 
 UUE5T5BoxTriggerComponent::UUE5T5BoxTriggerComponent()
 {
+    PrimaryComponentTick.bCanEverTick = true; // Enable ticking for this component
+
+    SetGenerateOverlapEvents(true); // Enable overlap events
 }
 
 void UUE5T5BoxTriggerComponent::BeginPlay()
 {
     Super::BeginPlay();
+    OnComponentBeginOverlap.AddDynamic(this, &UUE5T5BoxTriggerComponent::OnOverlapBegin);
+    OnComponentEndOverlap.AddDynamic(this, &UUE5T5BoxTriggerComponent::OnOverlapEnd);
+
+}
+
+void UUE5T5BoxTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+    FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    UE_LOG(LogTemp, Warning, TEXT("Tick"));
+}
+
+void UUE5T5BoxTriggerComponent::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                                               UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    
+
+    if (OtherActor != nullptr && OtherActor->ActorHasTag("UE5T5_BOX_PICKUP"))
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Overlap Begin: %s"), *OtherActor->GetName());
+        const TObjectPtr<AUE5T5GrabObj> GrabObj = Cast<AUE5T5GrabObj>(OtherActor);
+        if (GrabObj)
+        {
+            GrabbedObjects.Add(GrabObj);
+        }
+    }
+    UE_LOG(LogTemp, Warning, TEXT("GrabbedObjects: %d"), GrabbedObjects.Num());
+}
+
+void UUE5T5BoxTriggerComponent::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+    
+
+    if (OtherActor != nullptr && OtherActor->ActorHasTag("UE5T5_BOX_PICKUP"))
+    {
+        const TObjectPtr<AUE5T5GrabObj> GrabObj = Cast<AUE5T5GrabObj>(OtherActor);
+        if (GrabObj && GrabbedObjects.Contains(GrabObj))
+        {
+            GrabbedObjects.Remove(GrabObj);
+            UE_LOG(LogTemp, Warning, TEXT("Removed from GrabbedObjects: %s"), *OtherActor->GetName());
+        }
+        else
+        {
+            UE_LOG(LogTemp, Warning, TEXT("Actor not found in GrabbedObjects: %s"), *OtherActor->GetName());
+        }
+    }
+    UE_LOG(LogTemp, Warning, TEXT("GrabbedObjects: %d"), GrabbedObjects.Num());
 }
--- Source/UE5T5/Public/UE5T5BoxTriggerComponent.h
+++ Source/UE5T5/Public/UE5T5BoxTriggerComponent.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "UE5T5GrabObj.h"
 #include "Components/BoxComponent.h"
 #include "UE5T5BoxTriggerComponent.generated.h"
 
@@ -12,10 +13,26 @@
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class UE5T5_API UUE5T5BoxTriggerComponent : public UBoxComponent
 {
-   GENERATED_BODY()
-   UUE5T5BoxTriggerComponent();
+    GENERATED_BODY()
+    UUE5T5BoxTriggerComponent();
    
 protected:
-   virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+    
+    UFUNCTION()
+    void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent,
+        AActor* OtherActor,
+        UPrimitiveComponent* OtherComp,
+        int32 OtherBodyIndex,
+        bool bFromSweep,
+        const FHitResult& SweepResult);
+
+    UFUNCTION()
+    void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+private:
+    TArray<TObjectPtr<AUE5T5GrabObj>> GrabbedObjects;
 };
